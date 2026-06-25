36dfd8cfa4d4e0ec47df37257d873cd1caa71877
Gamebizixer <aparatspinner@gmail.com>
2026-06-25 15:39:50 +0330

    Update Presbalu.js

--- Presbalu.js
+++ Presbalu.js
@@ -2,9 +2,9 @@
 // scrollwroks.js - موتور بازی WebGL2 مستقل
 // CDN Address: https://cdn.jsdelivr.net/scrollstudio/u/scrollwroks.js
 // ================================================================
-cdn : https://cdn.jsdelivr.net/scrollstudio/u/scrollwroks.js
+Cdn :https://cdn.jsdelivr.net/scrollstudio/u/scrollwroks.js
 // این ثابت را می‌توانید برای بارگذاری Assets از همان CDN استفاده کنید
-const CDN_BASE = 'https://cdn.jsdelivr.net/scrollstudio/';
+const CDN_BASE = 'https://cdn.jsdelivr.net/scrollstudio/u/scrollwroks.js';
 
 (function(global) {
     'use strict';
